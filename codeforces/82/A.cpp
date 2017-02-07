3d97aeab216f29a9b8a53fb23250a6a3e1610826
Raihan Khan <raihankhanraka@googledreamer.com>
2017-02-07 19:44:00 +0600

    Add solution for problem `A - Double Cola` Link: https://codeforces.com/contest/82/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/82/submission/24489114

--- codeforces/82/A.cpp
+++ codeforces/82/A.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-long long int n;
-string cola[ 6] = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"} ;
-cin >> n ;
+ int n;
+ string s[]={"Sheldon","Leonard","Penny","Rajesh","Howard"};
+cin>>n;
 while(n>5)
-    n=n/2 -2 ;
-cout << cola[n-1] << endl;
+n=n/2-2;
+cout << s[n-1]  << endl;;
 return 0;
 }
