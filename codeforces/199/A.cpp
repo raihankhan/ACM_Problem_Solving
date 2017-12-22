cfd8e5b27cef614bda402c209350d2002e79d7e1
Raihan Khan <raihankhanraka@googledreamer.com>
2017-12-22 17:31:00 +0600

    Add solution for problem `A - Hexadecimal's theorem` Link: https://codeforces.com/contest/199/problem/A Tags: brute force, constructive algorithms, implementation, number theory, *900 Ref: https://codeforces.com/contest/199/submission/33504850

---
+++ codeforces/199/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define veci vector<int>
+#define vecl vector<long long int>
+#define pb push_back
+
+using namespace std;
+
+int main()
+{
+    lli m;
+    cin >> m ;
+    cout << "0 0 " << m << endl;
+    return 0;
+}
+
+
