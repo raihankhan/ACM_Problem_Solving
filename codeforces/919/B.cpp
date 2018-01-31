cb3abef3667a7531debbd7b7f79db6fab3f61bd6
Raihan Khan <raihankhanraka@googledreamer.com>
2018-01-31 19:46:00 +0600

    Add solution for problem `B - Perfect Number` Link: https://codeforces.com/contest/919/problem/B Tags: binary search, brute force, dp, implementation, number theory, *1100 Ref: https://codeforces.com/contest/919/submission/34754345

---
+++ codeforces/919/B.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define PI              acos(-1)
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define READ(f)         freopen(f, "r", stdin)
+#define WRITE(f)        freopen(f, "w", stdout)
+#define lli             long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define veci            vector<int>
+#define vecl            vector<long long int>
+#define pb              push_back
+long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+using namespace std;
+
+int main()
+{
+    lli sum,i,m,k=1;
+    int n;
+    vecl v;
+
+    cin >> n ;
+    for(i=19; ; i+=9)
+    {
+        m=i;
+        sum=0;
+        while(m)
+        {
+            sum+=m%10;
+            m=m/10;
+        }
+        if(sum==10)
+        {
+            v.pb(i); k++;
+        }
+        if(k==n+1) break;
+    }
+    cout << v[n-1] << endl;
+
+    return 0;
+}
+
+
