f19bb40fc42ac3670237bc8ed7df6b9260511b36
Raihan Khan <raihankhanraka@googledreamer.com>
2018-02-19 23:00:00 +0600

    Add solution for problem `B - Fafa and the Gates` Link: https://codeforces.com/contest/935/problem/B Tags: implementation, *900 Ref: https://codeforces.com/contest/935/submission/35485169

---
+++ codeforces/935/B.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define PI              acos(-1)
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define READ(f)         freopen(f, "r", stdin)
+#define WRITE(f)        freopen(f, "w", stdout)
+#define lli             long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define veci            vector<int>
+#define vecl            vector<long long int>
+#define vecp            vector< pair<int,int> >
+#define pb              push_back
+
+long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+using namespace std;
+
+int main()
+{
+
+    int n,len,i,x=0,y=0,cost=0,side;
+    string s;
+    cin >> n ;
+    cin >> s ;
+    for(i=0;i<n;i++)
+    {
+
+        if(s[i]=='U')
+        {
+            y++;
+        }
+        else if(s[i]=='R')
+        {
+            x++;
+        }
+        if(i>0)
+        {
+           if(side==1 and x<y) cost++;
+           if(side==2 and x>y) cost++;
+        }
+        if(x>y) side=1;
+        if(x<y) side=2;
+
+
+    }
+
+    cout << cost << endl;
+
+    return 0;
+}
