85fb2ef6f65bb1f0d6aca2691ffdbb87d3670cff
Raihan Khan <raihankhanraka@googledreamer.com>
2018-03-05 01:06:00 +0600

    Add solution for problem `A - Friends Meeting` Link: https://codeforces.com/contest/931/problem/A Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/931/submission/35947753

---
+++ codeforces/931/A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define PI              acos(-1)
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define READ(f)         freopen(f, "r", stdin)
+#define WRITE(f)        freopen(f, "w", stdout)
+#define lli             long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define veci            vector<int>
+#define vecl            vector<long long int>
+#define vecp            vector< pair<int,int> >
+#define pb              push_back
+
+long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+using namespace std;
+
+int main()
+{
+    int a,b,dif,ans,n,m;
+    cin >> a >> b ;
+    dif=abs(a-b);
+    if(dif<=2)
+        ans=dif;
+    else
+    {
+        n=dif/2;
+
+        if(dif%2==0)
+        ans=(n*(n+1));
+        else
+        ans=(n+1)*(n+1);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
