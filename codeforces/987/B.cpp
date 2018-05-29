01d32d396cc0a8a3c8ef7c0da048acd87d2e8399
Raihan Khan <raihankhanraka@googledreamer.com>
2018-05-29 22:15:00 +0600

    Add solution for problem `B - High School: Become Human` Link: https://codeforces.com/contest/987/problem/B Tags: math, *1100 Ref: https://codeforces.com/contest/987/submission/38735925

---
+++ codeforces/987/B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+#define PI                          acos(-1)
+#define min3(a,b,c)         min(a,min(b,c))
+#define max3(a,b,c)        max(a,max(b,c))
+#define   READ(f)             freopen(f, "r", stdin)
+#define  WRITE(f)            freopen(f, "w", stdout)
+#define               lli             long long int
+#define              ull            unsigned long long int
+#define              pii            pair < int, int>
+#define              pll            pair < ll, ll>
+#define              sc            scanf
+#define       scin(x)           sc("%d",&(x))
+#define       scln(x)           sc("%lld",&(x))
+#define              pf             printf
+#define          veci            vector<int>
+#define          vecl            vector<long long int>
+#define         vecp            vector<  pair<int,int> >
+#define             pb            push_back
+#define             inf            1<<30
+#define            mp            make_pair
+#define             ss            stringstream
+#define         all(v)           v.begin(), v.end()
+#define mem(x,y)            memset(x,y,sizeof(x))
+#define             M            1000000007
+long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+using namespace std;
+
+int main()
+{
+    double x,y,a,b;
+    cin >> x >> y ;
+    a=y*log(x);
+    b=x*log(y);
+    if(x==y or a==b)
+        cout << "=" << endl;
+    else if (a>b)
+        cout << ">" << endl;
+    else cout << "<" << endl;
+
+
+
+
+    return 0;
+}
