51a2ee5289dfbe0aef67095840b3fbf218f236c4
Raihan Khan <raihankhanraka@googledreamer.com>
2018-06-18 16:11:00 +0600

    Add solution for problem `A - Boredom` Link: https://codeforces.com/contest/455/problem/A Tags: dp, *1500 Ref: https://codeforces.com/contest/455/submission/39353056

---
+++ codeforces/455/A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+#define       PI                        acos(-1)
+#define       min3(a,b,c)        min(a,min(b,c))
+#define       max3(a,b,c)       max(a,max(b,c))
+#define       READ(f)             freopen(f, "r", stdin)
+#define       WRITE(f)            freopen(f, "w", stdout)
+#define       lli                         long long int
+#define       ull                        unsigned long long int
+#define       pii                       pair < int, int>
+#define       pll                        pair < ll, ll>
+#define       sc                       scanf
+#define       scin(x)                sc("%d",&(x))
+#define       scln(x)                sc("%lld",&(x))
+#define       pf                        printf
+#define       veci                    vector<int>
+#define       vecl                    vector<long long int>
+#define       vecp                   vector<  pair<int,int> >
+#define       pb                      push_back
+#define       inf                      1<<30
+#define       mp                     make_pair
+#define       ss                      stringstream
+#define       all(v)                  v.begin(), v.end()
+#define       mem(x,y)          memset(x,y,sizeof(x))
+#define       FastRead         ios_base::sync_with_stdio(0);cin.tie(0)
+#define       M                      1000000007
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+using namespace std;
+lli arr[100006]={0};
+int main()
+{
+    int n,i,j,k;
+    lli maxi,p;
+    scin(n);
+    for(i=0;i<n;i++) { scin(p); arr[p]++; }
+    for(i=1;i<= 100003 ;i++)
+    {
+        if(i-2<0) p=0;
+        else p=arr[i-2];
+
+        maxi=max( arr[i]*i + p , arr[i-1] );
+        arr[i]=maxi;
+    }
+
+    cout << maxi << endl;
+
+    return 0;
+}
