6a3d21da609d5b0c45c04f9e9a23eddedf4cc182
Raihan Khan <raihankhanraka@googledreamer.com>
2018-08-29 13:56:00 +0600

    Add solution for problem `A - Find Square` Link: https://codeforces.com/contest/1028/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1028/submission/42233164

---
+++ codeforces/1028/A.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+///...................................*****.................................................///
+///        Author :  Raihan Khan Raka  ( raihankhanraka@gmail.com )                         ///
+///                  Department of Computer Science                                         ///
+///                  & Engineering                                                          ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+#define       PI                   acos(-1)
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       READ(f)              freopen(f, "r", stdin)
+#define       WRITE(f)             freopen(f, "w", stdout)
+#define       lli                  long long int
+#define       ull                  unsigned long long int
+#define       pii                  pair < int, int>
+#define       pll                  pair < ll, ll>
+#define       sc                   scanf
+#define       scin(x)              sc("%d",&(x))
+#define       scln(x)              sc("%lld",&(x))
+#define       pf                   printf
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       vecp                 vector<  pair<int,int> >
+#define       pb                   push_back
+#define       inf                  1<<30
+#define       mp                   make_pair
+#define       ss                   stringstream
+#define       all(v)               v.begin(), v.end()
+#define       mem(x,y)             memset(x,y,sizeof(x))
+#define       FastRead             ios_base::sync_with_stdio(0);cin.tie(0)
+#define       M                    1000000007
+#define       rep( i , a , b )     for( i=a ; i<b ; i++)
+#define       rev( i , a , b )     for( i=a ; i>b ; i--)
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+/*
+int dx[5] = {1, -1, 0, 0 };
+int dy[5] = {0, 0, 1, -1};
+*/
+
+using namespace std;
+
+int main()
+{
+    int n,m,i,j,k,black=0,x,y;
+    char arr[200][200];
+
+    set< pair<int,int> > s;
+
+    cin >> n >> m ;
+    rep(i , 1 , n+1)
+    rep(j , 1 , m+1)
+    {
+        cin >> arr[i][j] ;
+        if(arr[i][j]=='B')
+           {
+                s.insert( mp(i , j) );
+                black++;
+           }
+    }
+
+    black= (black+1) >> 1 ;
+    i=0;
+    for( auto it: s)
+    {
+        i++;
+        if(i==black)
+        {
+            cout << it.first << " " << it.second << endl;
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
