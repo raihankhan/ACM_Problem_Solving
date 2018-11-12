99d902a8df491ed377929842a009b24ae5128437
Raihan Khan <raihankhanraka@googledreamer.com>
2018-11-12 23:58:00 +0600

    Add solution for problem `C - Meme Problem` Link: https://codeforces.com/contest/1076/problem/C Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/1076/submission/45633650

---
+++ codeforces/1076/C.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+///...................................*****.................................................///
+///        Author :  Raihan Khan Raka  ( raihankhanraka@gmail.com )                         ///
+///                  Department of Computer Science                                         ///
+///                  & Engineering                                                          ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+/*....................................Values................................................*/
+#define       inf                  1<<30
+#define       p5                   100007
+#define       p6                   1000007
+#define       PI                   acos(-1)
+#define       M                    1000000007
+
+/*....................................Functions.............................................*/
+#define       sqr(x)               x*x
+#define       sc                   scanf
+#define       pf                   printf
+#define       scin(x)              sc("%d",&(x))
+#define       scin2(x,y)            sc("d",&(x),&(y))
+#define       scin3(x,y,z)          sc("d%d",&(x),&(y),&(z))
+#define       scln(x)              sc("%lld",&(x))
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       all(v)               v.begin(), v.end()
+#define       ok                   cout << "ok" << endl
+#define       mem(x,y)             memset(x,y,sizeof(x))
+#define       READ(f)              freopen(f, "r", stdin)
+#define       WRITE(f)             freopen(f, "w", stdout)
+
+/*...................................Data_Types............................................*/
+#define       lli                  long long int
+#define       ull                  unsigned long long int
+#define       pii                  pair < int, int>
+#define       pll                  pair < ll, ll>
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       vecp                 vector<  pair<int,int> >
+#define       mapstrint            map< string , int >
+#define       mapstrstr            map< string , string >
+#define       mapint               map< int, int >
+#define       uset                 unordered_set
+#define       umap                 unordered_map
+#define       pq                   priority_queue
+
+#define       pb                   push_back
+#define       mp                   make_pair
+#define       ss                   stringstream
+
+/*.....................................Loops...............................................*/
+#define       rep( i , a , b )     for( i=a ; i<b ; i++)
+#define       rev( i , a , b )     for( i=a ; i>=b ; i--)
+#define       repx( i ,a,b, x)     for( i=a ; i<b ; i+=x)
+
+#define       FastRead             ios_base::sync_with_stdio(0);cin.tie(0)
+
+
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}*/
+/*
+int dx[5] = {1, -1, 0, 0 };
+int dy[5] = {0, 0, 1, -1};
+*/
+
+using namespace std;
+
+long double a, b,d ;
+long double func(long double x)
+{
+    long double ans;
+    a=x,b=d-x;
+    ans = (a+b)-(a*b);
+    return ans;
+
+}
+
+long double binsearch(long double d)
+{
+    long double l=0, r= d, mid, res;
+
+    while(r-l > 0.0000000001)
+    {
+        mid = (r+l)/2.0;
+        if( func(l)*func(mid) <= 0 )
+            r=mid;
+        else l=mid;
+
+    }
+    return (r+l)/2.0;
+}
+
+int main()
+{
+    int t,i;
+    scin(t);
+
+    rep(i, 0, t)
+    {
+        long double ans;
+        cin >> d;
+        ans = binsearch(d);
+
+        if(d==1 or d==2 or d==3)
+            cout << "N" << endl;
+        else
+            cout << "Y " << setprecision(12) << fixed << d-ans << " " << ans << endl;
+    }
+
+
+
+    return 0;
+}
+
