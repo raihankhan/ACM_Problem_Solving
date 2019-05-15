3677321a0b6f3ee5a7d3409ae662d40ca6d3d1b9
Raihan Khan <raihankhanraka@googledreamer.com>
2019-05-15 22:31:00 +0600

    Add solution for problem `C - News Distribution` Link: https://codeforces.com/contest/1167/problem/C Tags: dfs and similar, dsu, graphs, *1400 Ref: https://codeforces.com/contest/1167/submission/54207342

---
+++ codeforces/1167/C.cpp
@@ -0,0 +1,171 @@
+#include<bits/stdc++.h>
+///...................................*****.................................................///
+///        Author :  Raihan Khan Raka  ( raihankhanraka@gmail.com )                         ///
+///                  Department of Computer Science                                         ///
+///                  & Engineering                                                          ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+/*....................................Values................................................*/
+#define       inf                  1<<30
+#define       p5                   100007
+#define       p6                   1000007
+#define       PI                   acos(-1)
+#define       M                    1000000007
+#define       white                0
+#define       gray                 1
+#define       black                2
+/*....................................Functions.............................................*/
+#define       sqr(x)               x*x
+#define       sc                   scanf
+#define       pf                   printf
+#define       scin(x)              sc("%d",&(x))
+#define       scin2(x,y)           sc("d",&(x),&(y))
+#define       scin3(x,y,z)         sc("d%d",&(x),&(y),&(z))
+#define       scln(x)              sc("%lld",&(x))
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       all(v)               v.begin(), v.end()
+#define       ok                   cout << "ok" << endl
+#define       mem(x,y)             memset(x,y,sizeof(x))
+#define       READ(f)              freopen(f, "r", stdin)
+#define       WRITE(f)             freopen(f, "w", stdout)
+
+/*...................................Data_Types............................................*/
+#define       lli                  long long int
+#define       ull                  unsigned long long int
+#define       pii                  pair < int, int>
+#define       pll                  pair < ll, ll>
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       vecp                 vector<  pair<int,int> >
+#define       mapstrint            map< string , int >
+#define       mapstrstr            map< string , string >
+#define       mapint               map< int, int >
+#define       uset                 unordered_set
+#define       umap                 unordered_map
+#define       pq                   priority_queue
+
+#define       pb                   push_back
+#define       mp                   make_pair
+#define       ss                   stringstream
+
+/*.....................................Loops...............................................*/
+#define       rep( i , a , b )     for( i=a ; i<b ; i++)
+#define       rev( i , a , b )     for( i=a ; i>=b ; i--)
+#define       repx( i ,a,b, x)     for( i=a ; i<b ; i+=x)
+
+#define       IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}
+lli bigmod(lli n, lli k)
+{
+    lli ans=1;
+    while(k)
+    {
+        if(k&1)
+            ans=(ans*n)%M;
+        k=k>>1;
+        n=(n*n)%M;
+    }
+
+    return ans;
+}
+*/
+/*
+int dx4[5] = {1, -1, 0, 0 };
+int dy4[5] = {0, 0, 1, -1};
+int dx8[9] = { 0 , 0 , -1 , 1 , -1 , -1 , 1 , 1 } ;
+int dy8[9] = { -1 , 1 , 0 , 0 , -1 , 1 , -1 , 1 } ;
+
+int knightx[9] = { -1 , 1 , -2 , 2 , -2 , 2 , -1 , 1 } ;
+int knighty[9] = { -2 , -2 , -1 , -1 , 1 , 1 , 2 , 2 } ;
+
+bool valid( int r , int c , int x , int y ){ if( x >= 1 && x <= r && y >= 1 && y <= c ) return 1 ; return 0 ; }
+*/
+
+using namespace std;
+
+veci graph[5*p5];
+bool visited[5*p5];
+bool visited2[5*p5];
+int ans[5*p5];
+int n, e, k ,tot;
+
+void dfs1( int src )
+{
+    int i;
+    visited[src] = true;
+    tot++;
+    rep( i, 0, graph[src].size()  )
+    {
+        if( visited[ graph[src][i] ] == false)
+        {
+            dfs1( graph[src][i] );
+        }
+    }
+}
+void dfs2( int src )
+{
+    int i;
+    visited2[src] = true;
+    ans[src]=tot;
+    rep( i, 0, graph[src].size()  )
+    {
+        if( visited2[ graph[src][i] ] == false)
+        {
+            dfs2( graph[src][i] );
+        }
+    }
+}
+
+void edge( int u, int v)
+{
+    graph[u] . pb(v);
+    graph[v] . pb(u);
+}
+
+int main()
+{
+    int u,v,i;
+
+    scin(n);
+    scin(k);
+    while(k--)
+    {
+        scin(e);
+        if(e)
+            scin(u);
+        if(e>1)
+            rep(i, 1, e )
+        {
+            scin(v);
+            edge(u,v);
+            u=v;
+        }
+    }
+
+    rep(i , 1 , n+1)
+    {
+        if(visited[i]==false)
+        {
+            tot=0;
+            dfs1(i);
+            dfs2(i);
+        }
+    }
+
+    rep(i , 1 , n+1)
+    pf("%d ",ans[i]);
+
+    pf("\n");
+
+    return 0;
+}
