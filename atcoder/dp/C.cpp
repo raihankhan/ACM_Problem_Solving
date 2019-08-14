99c618bd471683a10c16ed55f90d88e19bf4d525
raihankhan <raihankhanraka@gmail.com>
2019-08-15 00:33:00 +0600

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/6909383

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,29 +1,135 @@
 #include<bits/stdc++.h>
-#define p5 100005
-using namespace std;
+///...................................*****.................................................///
+///        Author :  Raihan Khan Raka  ( raihankhanraka@gmail.com )                         ///
+///                  Department of Computer Science                                         ///
+///                  & Engineering                                                          ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
 
-int dp[p5][4],a[p5][4];
-int n;
+/*....................................Values................................................*/
+#define       p5                   100007
+#define       p6                   1000007
+#define       PI                   acos(-1)
+#define       M                    1000000007
+#define       inf                  1LL << 62
+#define       white                0
+#define       gray                 1
+#define       black                2
+/*....................................Functions.............................................*/
+#define       sqr(x)               x*x
+#define       sc                   scanf
+#define       pf                   printf
+#define       pfn                  printf("\n)
+#define       scin(x)              sc("%d",&(x))
+#define       scin2(xx,zz)         scanf("%d %d",&xx,&zz)
+#define       scln(x)              sc("%lld",&(x))
+#define       scln2(xx,zz)         scanf("%lld %lld",&xx,&zz)
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       all(v)               v.begin(), v.end()
+#define       ok                   cout << "ok" << endl
+#define       mem(x,y)             memset(x,y,sizeof(x))
+#define       clr(a)               a.clear()
+#define       READ(f)              freopen(f, "r", stdin)
+#define       WRITE(f)             freopen(f, "w", stdout)
 
-int solve(int day,int state)
+/*...................................Data_Types............................................*/
+#define       lli                  long long int
+#define       ull                  unsigned long long int
+#define       pii                  pair < int, int>
+#define       pll                  pair < ll, ll>
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       vecp                 vector<  pair<int,int> >
+#define       mapstrint            map< string , int >
+#define       mapstrstr            map< string , string >
+#define       mapint               map< int, int >
+#define       uset                 unordered_set
+#define       umap                 unordered_map
+#define       pq                   priority_queue
+
+#define       pb                   push_back
+#define       mp                   make_pair
+#define       ff                   first
+#define       ss                   second
+
+/*.....................................Loops...............................................*/
+#define       rep( i , a , b )     for( i=a ; i<b ; i++)
+#define       rev( i , a , b )     for( i=a ; i>=b ; i--)
+#define       repx( i ,a,b, x)     for( i=a ; i<b ; i+=x)
+
+#define       IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//long power(long int x, long int y){ int temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+/*lli gcd(lli x,lli y)
 {
-    if(day==n) return a[day][state];
-    if(dp[day][state]!=-1) return dp[day][state];
-    int cost=a[day][state];
-    if(state==1) cost+=max(solve(day+1,2),solve(day+1,3));
-    else if(state==2) cost+=max(solve(day+1,1),solve(day+1,3));
-    else if(state==3) cost+=max(solve(day+1,1),solve(day+1,2));
-    else cost=max( solve(day+1,1), max(solve(day+1,2),solve(day+1,3)) );
-
-    return dp[day][state]=cost;
+if(x==0) return y;
+return gcd(y%x,x);
+}
+lli bigmod(lli n, lli k)
+{
+lli ans=1;
+while(k)
+{
+if(k&1)
+ans=(ans*n)%M;
+k=k>>1;
+n=(n*n)%M;
 }
 
+return ans;
+}
+*/
+/*
+int dx4[5] = {1, -1, 0, 0 };
+int dy4[5] = {0, 0, 1, -1};
+int dx8[9] = { 0 , 0 , -1 , 1 , -1 , -1 , 1 , 1 } ;
+int dy8[9] = { -1 , 1 , 0 , 0 , -1 , 1 , -1 , 1 } ;
+
+int knightx[9] = { -1 , 1 , -2 , 2 , -2 , 2 , -1 , 1 } ;
+int knighty[9] = { -2 , -2 , -1 , -1 , 1 , 1 , 2 , 2 } ;
+
+bool valid( int r , int c , int x , int y ){ if( x >= 1 && x <= r && y >= 1 && y <= c ) return 1 ; return 0 ; }
+*/
+
+using namespace std;
+lli dp[4][p5],vr[4][p5],n;
+
+lli f(lli state , lli pos)
+{
+    if(dp[state][pos]) return dp[state][pos];
+    if(pos==n) return vr[state][pos];
+    lli p=state%3;
+    lli q=(p==1)? 3 : ((p==2)? 1 : 2);
+    return dp[state][pos]=vr[state][pos] + max ( f(p+1 , pos+1 ) , f( q , pos+1) ) ;
+}
 int main()
 {
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++) scanf("%d %d %d",&a[i][1],&a[i][2],&a[i][3]);
-    memset(dp,-1,sizeof(dp));
-    printf("%d\n",solve(0,0));
+    lli ans,i,j,k,p;
 
+    scln(n);
+
+    rep(i , 1 , n+1)
+    {
+        scln2(vr[1][i],vr[2][i]),scln(vr[3][i]);
+
+    }
+    ans=0;
+    ans=max(ans,f(1,1));
+    ans=max(ans,f(2,1));
+    ans=max(ans,f(3,1));
+
+    pf("%lld\n",ans);
+
+
+
+
+#ifdef HOME
+    cerr << "Time elapsed: " << clock() / 1000 << "
+         ms" << endl;
+#endif
     return 0;
 }
+
