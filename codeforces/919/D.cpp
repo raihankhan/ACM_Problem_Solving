dc969cffb68d48fc1866a127c32fa395635532b3
Raihan Khan <raihankhanraka@googledreamer.com>
2020-05-15 21:53:00 +0600

    Add solution for problem `D - Substring` Link: https://codeforces.com/contest/919/problem/D Tags: dfs and similar, dp, graphs, *1700 Ref: https://codeforces.com/contest/919/submission/80242114

---
+++ codeforces/919/D.cpp
@@ -0,0 +1,169 @@
+#include<bits/stdc++.h>
+///...................................*****.................................................///
+///        Author :  Raihan Khan Raka  ( raihankhanraka@gmail.com )                         ///
+///                  Department of Computer Science                                         ///
+///                  & Engineering                                                          ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+/*....................................Values................................................*/
+#define       p5                   100007
+#define       p6                   1000007
+#define       PI                   acos(-1)
+#define       M                    1000000007
+#define       inf                  1LL << 62
+#define       white                0
+#define       gray                 1
+#define       black                2
+/*....................................Functions.............................................*/
+#define       sqr(x)               x*x
+#define       sc                   scanf
+#define       pf                   printf
+#define       pfn                  printf("\n")
+#define       scin(x)              sc("%d",&(x))
+#define       scin2(xx,zz)         scanf("%d %d",&xx,&zz)
+#define       scln(x)              sc("%lld",&(x))
+#define       scln2(xx,zz)         scanf("%lld %lld",&xx,&zz)
+#define       min3(a,b,c)          min(a,b<c?b:c)
+#define       max3(a,b,c)          max(a,b>c?b:c)
+#define       all(v)               v.begin(), v.end()
+#define       ok                   cout << "ok" << endl
+#define       mem(x,y)             memset(x,y,sizeof(x))
+#define       clr(a)               a.clear()
+#define       READ(f)              freopen(f, "r", stdin)
+#define       WRITE(f)             freopen(f, "w", stdout)
+
+/*...................................Data_Types............................................*/
+#define       lli                  long long int
+#define       ull                  unsigned long long int
+#define       pii                  pair < int, int>
+#define       pll                  pair < ll, ll>
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       vecp                 vector<  pair<int,int> >
+#define       mapstrint            map< string , int >
+#define       mapstrstr            map< string , string >
+#define       mapint               map< int, int >
+#define       uset                 unordered_set
+#define       umap                 unordered_map
+#define       pq                   priority_queue
+
+#define       pb                   push_back
+#define       mp                   make_pair
+#define       ff                   first
+#define       ss                   second
+
+/*.....................................Loops...............................................*/
+#define       rep( i , a , b )     for( i=a ; i<b ; i++)
+#define       rev( i , a , b )     for( i=a ; i>=b ; i--)
+#define       repx( i ,a,b, x)     for( i=a ; i<b ; i+=x)
+
+#define       IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+//int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+///------------------------------- Mudular functions----------------------------------------
+/*
+inline lli power(lli x, lli y){ lli temp; if( y == 0) return 1; temp = power(x, y/2); if (y%2 == 0) return temp*temp; else return x*temp*temp; }
+inline lli add(lli a, lli b) {a += b; return a >= M ? a - M : a;}
+inline lli sub(lli a, lli b) {a -= b; return a < 0 ? a + M : a;}
+inline lli mul(lli a, lli b) {return  (a * b) % M;}
+lli gcd(lli x,lli y)
+{
+    if(x==0) return y;
+    return gcd(y%x,x);
+}
+lli bigmod(lli n, lli k )
+{
+    lli ans=1;
+    while(k)
+    {
+        if(k&1)
+            ans=(ans*n)%M;
+        k=k>>1;
+        n=(n*n)%M;
+    }
+
+    return ans;
+}
+*/
+///----------------------------------Graph moves----------------------------------------
+/*
+int dx4[5] = {1, -1, 0,  0};
+int dy4[5] = {0,  0, 1, -1};
+int dx8[9] = {0,  0, 1, -1, -1, 1, -1,  1};
+int dy8[9] = {-1, 1, 0,  0,  1, 1, -1, -1};
+int knightx[9] = {-2, -2, -1, -1,  1, 1, 2,  2};
+int knighty[9] = {-1,  1, -2,  2, -2, 2, -1, 1};
+bool valid( int r , int c , int x , int y ){ if( x >= 1 && x <= r && y >= 1 && y <= c ) return 1 ; return 0 ; }
+*/
+
+using namespace std;
+string s;
+veci g[3*p5];
+int col[3*p5];
+int val[3*p5][28];
+
+void dfs(int u)
+{
+    col[u]=gray;
+
+    int j;
+    for(auto v:g[u])
+    {
+        if(col[v]==white)
+            dfs(v);
+        else if(col[v]==gray)
+        {
+            pf("-1\n");
+            exit(0);
+        }
+
+        rep(j , 1 , 27)
+        {
+            val[u][j]=max(val[u][j],val[v][j]);
+        }
+    }
+
+    val[u][s[u]-'a'+1]++;
+    col[u]=black;
+}
+
+int main()
+{
+    int n,m,i,j,k,p,u,v;
+
+    scin2(n,m);
+    cin >> s ;
+    s=" "+s;
+    rep(i , 0 , m)
+    {
+        scin2(u,v);
+        g[u].pb(v);
+    }
+
+    rep(i , 1 , n+1)
+    {
+        if(!col[i])
+        {
+            dfs(i);
+        }
+    }
+
+    int maxival=0;
+    rep(i , 1 , n+1)
+    {
+        rep(j , 1, 27)
+        {
+            maxival=max(maxival,val[i][j]);
+        }
+    }
+
+    pf("%d\n",maxival);
+
+#ifdef HOME
+    cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
+
